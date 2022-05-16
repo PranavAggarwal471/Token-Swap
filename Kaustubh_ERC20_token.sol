29d03ffcdde38cda4e3c08208ec04fc3d9213763
Pranav Aggarwal <89070024+PranavAggarwal471@users.noreply.github.com>
2022-05-16 18:39:49 +0530

    Create Kaustubh_ERC20_token.sol

---
+++ Kaustubh_ERC20_token.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract Token_KAU is ERC20 {
+     constructor() ERC20("Kaustubh", "KAU") {
+        // Mint 100 tokens to msg.sender
+        _mint(msg.sender, 100);
+    }
+} 
